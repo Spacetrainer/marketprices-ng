ce653b5865c449a08ce5fd546934b852f7ebd564
Abolaji <abolajiosilowo@gmail.com>
2026-05-19 12:44:55 +0000

    Initial Next.js setup with marketprices.ng homepage

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center p-8">
+      <h1 className="text-4xl font-medium mb-2">marketprices.ng</h1>
+      <p className="text-gray-600">Nigerian food markets, in one place.</p>
+    </main>
+  );
+}
